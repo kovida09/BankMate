a9ef32347f81e3d85d09ec22ba61382e39ce226f
Smile <singhswapnil060303@gmail.com>
2024-07-23 22:58:54 +0530

    chore: Add proxy configuration for API requests and update Dashboard component with navigation links

--- banking_frontend/src/components/Dashboard.js
+++ banking_frontend/src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <h1>Dashboard</h1>
       {/* Add dashboard content here */}
+      <div className="options">
+        <Link to="/customer-information">Customer Information</Link>
+        <Link to="/credit-card-transactions">Credit Card Transactions</Link>
+        <Link to="/loan-information">Loan Information</Link>
+        <Link to="/account-information">Account Information</Link>
+      </div>
     </div>
   );
 };
